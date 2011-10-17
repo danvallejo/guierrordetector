7a304544380312408020b648e28215c8a3575498
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2011-10-17 19:22:07 +0000

    Add a new constructor to CGBuilder. Add common eclipse plugin constants. Clean unused globals vars.

--- trunk/GUIErrorDetector/src/edu/washington/cs/detector/CGBuilder.java
+++ trunk/GUIErrorDetector/src/edu/washington/cs/detector/CGBuilder.java
@@ -1,7 +1,13 @@
 package edu.washington.cs.detector;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
 
+import com.ibm.wala.classLoader.IClass;
+import com.ibm.wala.classLoader.IMethod;
+import com.ibm.wala.core.tests.callGraph.CallGraphTestUtil;
 import com.ibm.wala.ipa.callgraph.AnalysisCache;
 import com.ibm.wala.ipa.callgraph.AnalysisOptions;
 import com.ibm.wala.ipa.callgraph.AnalysisScope;
@@ -10,10 +16,15 @@ import com.ibm.wala.ipa.callgraph.CallGraph;
 import com.ibm.wala.ipa.callgraph.CallGraphBuilder;
 import com.ibm.wala.ipa.callgraph.CallGraphStats;
 import com.ibm.wala.ipa.callgraph.Entrypoint;
+import com.ibm.wala.ipa.callgraph.impl.DefaultEntrypoint;
 import com.ibm.wala.ipa.callgraph.impl.Util;
 import com.ibm.wala.ipa.cha.ClassHierarchy;
+import com.ibm.wala.types.ClassLoaderReference;
+import com.ibm.wala.types.MethodReference;
+import com.ibm.wala.util.collections.HashSetFactory;
 import com.ibm.wala.util.config.AnalysisScopeReader;
 import com.ibm.wala.util.graph.Graph;
+import com.ibm.wala.util.io.FileProvider;
 
 import edu.washington.cs.detector.util.WALAUtils;
 
@@ -21,6 +32,11 @@ public class CGBuilder {
 	
 	public final String appPath;
 	public final File exclusionFile;
+	
+	public CGBuilder(String appPath) throws IOException {
+		this.appPath = appPath;
+		this.exclusionFile = FileProvider.getFile(CallGraphTestUtil.REGRESSION_EXCLUSIONS);
+	}
 	
 	public CGBuilder(String appPath, File exclusionFile) {
 		this.appPath = appPath;
@@ -30,11 +46,12 @@ public class CGBuilder {
 	private CallGraph callgraph = null;
 	private ClassHierarchy cha = null;
 	private Graph<CGNode> appCallGraph = null;
+	private AnalysisScope scope = null;
 	
 	public void buildCG() {
 		try {
 			// get all files for analysis
-			AnalysisScope scope = AnalysisScopeReader
+			this.scope = AnalysisScopeReader
 					.makeJavaBinaryAnalysisScope(appPath, exclusionFile);
 			this.cha = ClassHierarchy.make(scope);
 			// treat all main methods as entry points for call graph
@@ -65,6 +82,34 @@ public class CGBuilder {
 	
 	public Graph<CGNode> getAppCallGraph() {
 		return appCallGraph;
+	}
+	
+	public AnalysisScope getAnalysisScope() {
+		return scope;
+	}
+	
+	public static Iterable<Entrypoint> getCustomizedEntryPointsInApp(
+			AnalysisScope scope, ClassHierarchy cha, String methodClass, String methodName, String methodSignature) {
+		if (cha == null) {
+			throw new IllegalArgumentException("cha is null");
+		}
+		ClassLoaderReference clr = scope.getApplicationLoader();
+		
+		final HashSet<Entrypoint> result = HashSetFactory.make();
+		for (IClass klass : cha) {
+			if (klass.getClassLoader().getReference().equals(clr)) {
+				MethodReference methodRef = MethodReference.findOrCreate(clr, methodClass, methodName, methodSignature);
+				IMethod m = klass.getMethod(methodRef.getSelector());
+				if (m != null) {
+					result.add(new DefaultEntrypoint(m, cha));
+				}
+			}
+		}
+		return new Iterable<Entrypoint>() {
+			public Iterator<Entrypoint> iterator() {
+				return result.iterator();
+			}
+		};
 	}
 	
 }
---
+++ trunk/GUIErrorDetector/src/edu/washington/cs/detector/util/EclipsePluginCommons.java
@@ -0,0 +1,30 @@
+package edu.washington.cs.detector.util;
+
+public class EclipsePluginCommons {
+	
+	public static String PLUGIN_DIR = "D:\\develop-tools\\eclipse\\eclipse\\plugins\\";
+
+	public static String DEPENDENT_JARS =
+		PLUGIN_DIR + "org.eclipse.ui_3.6.2.M20110203-1100.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.swt_3.6.2.v3659c.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.swt.win32.win32.x86_64_3.6.2.v3659c.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.jface_3.6.2.M20110210-1200.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.core.commands_3.6.0.I20100512-1500.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.ui.workbench_3.6.2.M20110210-1200.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.core.runtime_3.6.0.v20100505.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.osgi_3.6.2.R36x_v20110210.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.equinox.common_3.6.0.v20100503.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.core.jobs_3.5.1.R36x_v20100824.jar" + Globals.pathSep +
+		PLUGIN_DIR + "\\org.eclipse.core.runtime.compatibility.registry_3.3.0.v20100520\\runtime_registry_compatibility.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.equinox.registry_3.5.0.v20100503.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.equinox.preferences_3.3.0.v20100503.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.core.contenttype_3.4.100.v20100505-1235.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.equinox.app_1.3.1.R36x_v20100803.jar";
+	
+	public static String DEPENDENT_UI_JARS =
+		PLUGIN_DIR + "org.eclipse.ui_3.6.2.M20110203-1100.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.jface_3.6.2.M20110210-1200.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.ui.workbench_3.6.2.M20110210-1200.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.core.runtime_3.6.0.v20100505.jar" + Globals.pathSep;
+	
+}
