b6df0d2b461b7f707eb754d773ea758c32616cbf
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2011-10-18 00:45:15 +0000

    refactor the CG builder

--- trunk/GUIErrorDetector/src/edu/washington/cs/detector/util/EclipsePluginCommons.java
+++ trunk/GUIErrorDetector/src/edu/washington/cs/detector/util/EclipsePluginCommons.java
@@ -21,10 +21,22 @@ public class EclipsePluginCommons {
 		PLUGIN_DIR + "org.eclipse.core.contenttype_3.4.100.v20100505-1235.jar" + Globals.pathSep +
 		PLUGIN_DIR + "org.eclipse.equinox.app_1.3.1.R36x_v20100803.jar";
 	
-	public static String DEPENDENT_UI_JARS =
+	public static String DEPENDENT_MIN_UI_JARS =
 		PLUGIN_DIR + "org.eclipse.ui_3.6.2.M20110203-1100.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.swt_3.6.2.v3659c.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.swt.win32.win32.x86_64_3.6.2.v3659c.jar" + Globals.pathSep +
 		PLUGIN_DIR + "org.eclipse.jface_3.6.2.M20110210-1200.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.core.commands_3.6.0.I20100512-1500.jar" + Globals.pathSep +
 		PLUGIN_DIR + "org.eclipse.ui.workbench_3.6.2.M20110210-1200.jar" + Globals.pathSep +
-		PLUGIN_DIR + "org.eclipse.core.runtime_3.6.0.v20100505.jar" + Globals.pathSep;
+		PLUGIN_DIR + "org.eclipse.core.runtime_3.6.0.v20100505.jar" + Globals.pathSep + 
+		PLUGIN_DIR + "org.eclipse.osgi_3.6.2.R36x_v20110210.jar" + Globals.pathSep + 
+		PLUGIN_DIR + "org.eclipse.core.commands_3.6.0.I20100512-1500.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.core.jobs_3.5.1.R36x_v20100824.jar" + Globals.pathSep +
+		PLUGIN_DIR + "\\org.eclipse.core.runtime.compatibility.registry_3.3.0.v20100520\\runtime_registry_compatibility.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.equinox.common_3.6.0.v20100503.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.equinox.registry_3.5.0.v20100503.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.equinox.preferences_3.3.0.v20100503.jar" + Globals.pathSep +
+		PLUGIN_DIR + "org.eclipse.equinox.app_1.3.1.R36x_v20100803.jar";
+
 	
 }
