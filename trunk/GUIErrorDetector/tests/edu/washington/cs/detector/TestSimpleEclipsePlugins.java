f7d293a7c4e23a4abdf9ffd998c2ce17136125aa
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2011-10-19 23:55:44 +0000

    renaming refactoring

---
+++ trunk/GUIErrorDetector/tests/edu/washington/cs/detector/TestSimpleEclipsePlugins.java
@@ -0,0 +1,29 @@
+package edu.washington.cs.detector;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.ibm.wala.ipa.cha.ClassHierarchyException;
+
+import junit.framework.TestCase;
+
+public class TestSimpleEclipsePlugins extends TestCase {
+	
+	public void testSimplePlugin() throws ClassHierarchyException, IOException {
+		EclipsePluginUIErrorMain.APP_PATH = "D:\\research\\guierror\\eclipsews\\plugintest\\bin";
+		EclipsePluginUIErrorMain.UI_CLASSES_FILE = "./tests/uiclasses_plugintest.txt";
+		
+		EclipsePluginUIErrorMain main = new EclipsePluginUIErrorMain();
+		List<AnomalyCallChain> chains = main.reportUIErrors();
+		
+		CallChainFilter filter = new CallChainFilter(chains);
+		List<AnomalyCallChain> afterFilter = filter.apply(new RemoveSystemCallStrategy());
+		System.out.println("Number of chain after filtering: " + afterFilter.size());
+		
+		for(AnomalyCallChain chain : afterFilter) {
+			System.out.println("---- after filtering----");
+			System.out.println(chain.getFullCallChainAsString());
+		}
+	}
+
+}
