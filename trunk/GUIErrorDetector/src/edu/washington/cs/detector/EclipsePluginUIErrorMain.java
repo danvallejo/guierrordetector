232e8c03dee02539275f545dabd2a071bdba1914
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2011-10-20 03:59:26 +0000

    Do refactoring

--- trunk/GUIErrorDetector/src/edu/washington/cs/detector/EclipsePluginUIErrorMain.java
+++ trunk/GUIErrorDetector/src/edu/washington/cs/detector/EclipsePluginUIErrorMain.java
@@ -1,15 +1,9 @@
 package edu.washington.cs.detector;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-
-import com.ibm.wala.ipa.callgraph.AnalysisScope;
 import com.ibm.wala.ipa.callgraph.Entrypoint;
-import com.ibm.wala.ipa.cha.ClassHierarchy;
 import com.ibm.wala.ipa.cha.ClassHierarchyException;
-import com.ibm.wala.util.collections.HashSetFactory;
 
 import edu.washington.cs.detector.util.EclipsePluginCommons;
 import edu.washington.cs.detector.util.Files;
@@ -41,7 +35,7 @@ public class EclipsePluginUIErrorMain {
 		CGBuilder builder = new CGBuilder(appPath);
 		builder.makeScopeAndClassHierarchy();
 		
-		Iterable<Entrypoint> entries = this.getAllPublicMethods(builder, uiClasses);
+		Iterable<Entrypoint> entries = CGEntryManager.getAllPublicMethods(builder, uiClasses);
 		System.out.println("All entries:");
 		int count = 0;
 		for(Entrypoint ep : entries) {
@@ -72,24 +66,5 @@ public class EclipsePluginUIErrorMain {
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-	}
-	
-	public Iterable<Entrypoint> getAllPublicMethods(CGBuilder builder, List<String> uiClasses) {
-		AnalysisScope scope = builder.getAnalysisScope();
-		ClassHierarchy cha = builder.getClassHierarchy();
-		
-		final HashSet<Entrypoint> result = HashSetFactory.make();
-		for(String methodClass : uiClasses) {
-		    Iterable<Entrypoint> entries = CGEntryManager.getAppPublicMethodsByClass(scope, cha, methodClass);
-		    for(Entrypoint ep : entries) {
-		    	result.add(ep);
-		    }
-		}
-		
-		return new Iterable<Entrypoint>() {
-			public Iterator<Entrypoint> iterator() {
-				return result.iterator();
-			}
-		}; 
 	}
 }
