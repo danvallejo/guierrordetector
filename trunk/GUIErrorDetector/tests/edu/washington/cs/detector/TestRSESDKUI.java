af338fdd73ac52393c2b9f4654ceb27632f71726
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2011-10-22 23:35:45 +0000

    add testing oracle and 1 known bug

--- trunk/GUIErrorDetector/tests/edu/washington/cs/detector/TestRSESDKUI.java
+++ trunk/GUIErrorDetector/tests/edu/washington/cs/detector/TestRSESDKUI.java
@@ -1,10 +1,12 @@
 package edu.washington.cs.detector;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.ibm.wala.classLoader.IClass;
 import com.ibm.wala.ipa.cha.ClassHierarchyException;
 
+import edu.washington.cs.detector.CGBuilder.CG;
 import edu.washington.cs.detector.util.EclipsePluginCommons;
 import edu.washington.cs.detector.util.Globals;
 
@@ -30,11 +32,35 @@ public class TestRSESDKUI extends AbstractUITest {
 	}
 
 	public void testGetAppJars() {
-		super.reportAppJars();
+		super.checkAppJarNumber(46);
 	}
 
 	public void testDetectUIErrors() throws IOException,
 			ClassHierarchyException {
-		super.reportUIErrors(SWTAppUIErrorMain.default_log);
+		List<AnomalyCallChain> chains = super.reportUIErrors(SWTAppUIErrorMain.default_log, CG.ZeroOneContainerCFA);
+		assertEquals(308, chains.size());
+	}
+	
+	//I can not reproduce this!
+	public void testKnownBug267478() throws ClassHierarchyException, IOException {
+		//org.eclipse.rse.services.dstore.util.DownloadListener
+		AbstractUITest test = new AbstractUITest(){
+			@Override
+			protected boolean isUIClass(IClass kclass) {
+				return kclass.toString().indexOf("org/eclipse/dstore/core/model/UpdateHandler") != -1;
+			}
+			@Override
+			protected String getAppPath() {
+				return PLUGIN_DIR;
+			}
+			@Override
+			protected String getDependentJars() {
+				return EclipsePluginCommons.DEPENDENT_JARS;
+			}
+			
+		};
+		AbstractUITest.DEBUG = true;
+		List<AnomalyCallChain> chains  = test.reportUIErrors(SWTAppUIErrorMain.default_log, CG.ZeroOneCFA);
+		assertEquals(0, chains.size());
 	}
 }
