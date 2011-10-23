b33d7e32317262239a527a446a8a6a05d3d9ae1f
racezhang <racezhang@gmail.com>
2011-10-23 19:44:21 +0000

    Add tests for testing configurations

--- GUIErrorDetector/src/edu/washington/cs/detector/util/Utils.java
+++ GUIErrorDetector/src/edu/washington/cs/detector/util/Utils.java
@@ -1,5 +1,6 @@
 package edu.washington.cs.detector.util;
 
+import java.io.File;
 import java.util.Collection;
 import java.util.LinkedList;
 
@@ -13,6 +14,36 @@ public class Utils {
 	public static String translateDotToSlash(String str) {
 		assert str != null;
 		return str.replace('.', '/');
+	}
+	
+	public static void checkDirExistence(String dir) {
+		File f = new File(dir);
+		if(!f.isDirectory()) {
+			throw new RuntimeException("File: " + f + " is not a dir");
+		}
+		if(!f.exists()) {
+			throw new RuntimeException("Dir: " + f + " does not exist");
+		}
+	}
+	
+	public static void checkFileExistence(String dir) {
+		File f = new File(dir);
+		if(f.isDirectory()) {
+			throw new RuntimeException("File: " + f + " is  a dir");
+		}
+		if(!f.exists()) {
+			throw new RuntimeException("File: " + f + " does not exist");
+		}
+	}
+	
+	public static void checkPathEntryExistence(String path) {
+		String[] entries = path.split(Globals.pathSep);
+		for(String entry : entries) {
+			File f = new File(entry);
+			if(!f.exists()) {
+				throw new RuntimeException("The entry: " + entry + " does not exist.");
+			}
+		}
 	}
 	
 	public static <T> boolean includedIn(T target, T[] array) {
