b55fdd2359502a92128d854bc943f9e020f63158
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2011-10-23 22:47:40 +0000

    add corresponding tests

--- trunk/GUIErrorDetector/tests/edu/washington/cs/detector/TestFileExistence.java
+++ trunk/GUIErrorDetector/tests/edu/washington/cs/detector/TestFileExistence.java
@@ -1,6 +1,10 @@
 package edu.washington.cs.detector;
 
+import java.io.IOException;
+import java.util.List;
+
 import edu.washington.cs.detector.util.EclipsePluginCommons;
+import edu.washington.cs.detector.util.Files;
 import edu.washington.cs.detector.util.Utils;
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -30,6 +34,17 @@ public class TestFileExistence extends TestCase {
 	
 	public void testPathExistence() {
 		Utils.checkPathEntryExistence(EclipsePluginCommons.DEPENDENT_JARS);
+	}
+	
+	public void testCheckingMethodFiles() throws IOException {
+		String fPath = "./src/checking_methods.txt";
+		Utils.checkFileExistence(fPath);
+		List<String> list = Files.readWhole(fPath);
+		System.out.println(list);
+		assertEquals(list.size(), UIAnomalyMethodFinder.checking_methods.length);
+		for(String str : UIAnomalyMethodFinder.checking_methods) {
+			assertTrue("str: " + str + " not in: " + fPath, list.contains(str));
+		}
 	}
 
 }
