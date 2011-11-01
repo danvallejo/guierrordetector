cb2c4a1fc287c1eb313a8d2efbc36f970ee2d08b
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2011-11-01 05:03:49 +0000

    the sonar experiment

---
+++ trunk/GUIErrorDetector/tests/edu/washington/cs/detector/experiments/TestSonar.java
@@ -0,0 +1,47 @@
+package edu.washington.cs.detector.experiments;
+
+import java.io.IOException;
+import java.util.List;
+
+import edu.washington.cs.detector.AnomalyCallChain;
+import edu.washington.cs.detector.CGBuilder;
+import edu.washington.cs.detector.CallChainFilter;
+import edu.washington.cs.detector.RemoveSystemCallStrategy;
+import edu.washington.cs.detector.SWTAppUIErrorMain;
+import edu.washington.cs.detector.UIAnomalyDetector;
+import edu.washington.cs.detector.CGBuilder.CG;
+import edu.washington.cs.detector.util.Globals;
+import edu.washington.cs.detector.util.Utils;
+import junit.framework.TestCase;
+
+public class TestSonar extends TestCase {
+	
+	/**
+	 * No bugs found
+	 * */
+	public void testRunningSonar() throws IOException {
+		String appPath = "D:\\research\\guierror\\subjects\\sonar.jar"
+			+ Globals.pathSep +  SWTAppUIErrorMain.swtJar
+			+ Globals.pathSep + "D:\\research\\guierror\\eclipsews\\Sonar\\mail.jar"
+			+ Globals.pathSep + "D:\\research\\guierror\\eclipsews\\Sonar\\htmlparser.jar"
+			+ Globals.pathSep + "D:\\research\\guierror\\eclipsews\\Sonar\\htmllexer.jar";;
+		UIAnomalyDetector detector = new UIAnomalyDetector(appPath);
+		
+		CGBuilder builder = new CGBuilder(appPath);
+		builder.setCGType(CG.OneCFA);
+		builder.buildCG();
+		
+		List<AnomalyCallChain> chains = detector.detectUIAnomaly(builder);
+		
+		System.out.println("Number of anomaly call chains: " + chains.size());
+		
+		CallChainFilter filter = new CallChainFilter(chains);
+		chains = filter.apply(new RemoveSystemCallStrategy());
+		System.out.println("No of chains after filtering system classes: " + chains.size());
+		
+		Utils.dumpAnomalyCallChains(chains, "./logs/sonar-anomalies.txt");
+		
+		assertTrue(chains.size() == 0);
+		
+	}
+}
