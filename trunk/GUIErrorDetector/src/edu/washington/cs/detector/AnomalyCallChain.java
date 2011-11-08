e3f87fc4c27e865fc5d91f1cf728f73b32ce21ce
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2011-11-08 02:13:34 +0000

    Add a utility method to check whether a given path is valid

--- trunk/GUIErrorDetector/src/edu/washington/cs/detector/AnomalyCallChain.java
+++ trunk/GUIErrorDetector/src/edu/washington/cs/detector/AnomalyCallChain.java
@@ -1,10 +1,12 @@
 package edu.washington.cs.detector;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.ibm.wala.ipa.callgraph.CGNode;
+import com.ibm.wala.util.graph.Graph;
 
 import edu.washington.cs.detector.util.Globals;
 
@@ -65,5 +67,65 @@ public class AnomalyCallChain {
 			sb.append(Globals.lineSep);
 		}
 		return sb.toString();
+	}
+	
+	/**
+	 * It is a utility method to check whether there is a valid call chain from
+	 * the startNode, then following strictly the given node signatures. Suppose
+	 * the startNode is "s", and nodeSigns = {a, b, c, d}. So this method will
+	 * return a call chain if there is a path s -> a -> b -> c -> d.
+	 * Otherwise, it returns null
+	 * */
+	public static AnomalyCallChain extractCallChainNodeByNode(Graph<CGNode> cg, CGNode startNode, 
+			String[] nodeSigs) {
+		
+		List<CGNode> nodesInPath = new LinkedList<CGNode>();
+		nodesInPath.add(startNode);
+		
+		CGNode currentNode = startNode;
+		for(String sig : nodeSigs) {
+		    Iterator<CGNode> it = cg.getSuccNodes(currentNode);
+		    //a list to keep all next nodes
+		    List<CGNode> foundNextNodes = new LinkedList<CGNode>();
+		    while(it.hasNext()) {
+		    	CGNode next = it.next();
+		    	if(next.toString().indexOf(sig) != -1) {
+		    		foundNextNodes.add(next);
+		    	}
+		    }
+		    //return if no nodes
+		    if(foundNextNodes.isEmpty()) {
+		    	//return null;
+		    	throw new RuntimeException("No nodes found: " + sig + ", after current node: "
+		    			+ currentNode);
+		    }
+		    //check the next nodes
+		    if(foundNextNodes.size() > 1) {
+		    	throw new RuntimeException("There should be at most 1 matched node for: "
+		    			+ sig + ". In fact, it has: " + foundNextNodes.size() + ", namely: "
+		    			+ foundNextNodes);
+		    }
+		    currentNode = foundNextNodes.get(0);
+		    nodesInPath.add(currentNode);
+		}
+		
+		//construct an anomaly call chain from a list of nodes
+		int startIndex = -1;
+		for(int i = 0; i < nodesInPath.size(); i++) {
+			CGNode node = nodesInPath.get(i);
+			if(node.getMethod().getSignature().equals(ThreadStartFinder.THREAD_START_SIG)) {
+				startIndex = i;
+				break;
+			}
+		}
+		if(startIndex == -1) {
+			throw new RuntimeException("No thread start method.");
+		}
+		
+		AnomalyCallChain chain = new AnomalyCallChain();
+		chain.addNodes(nodesInPath.subList(0, startIndex), nodesInPath.get(startIndex),
+				nodesInPath.subList(startIndex + 1, nodesInPath.size()));
+		
+		return chain;
 	}
 }
