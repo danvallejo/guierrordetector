cd119b0f4312ff492ad7f2cf97a4636085e36062
racezhang <racezhang@gmail.com>
2011-11-22 19:46:14 +0000

    merge the abstract UI testing common class

--- GUIErrorDetector/tests/edu/washington/cs/detector/experiments/TestJEdit.java
+++ GUIErrorDetector/tests/edu/washington/cs/detector/experiments/TestJEdit.java
@@ -1,5 +1,7 @@
 package edu.washington.cs.detector.experiments;
 
+import java.io.IOException;
+
 /**
  * Test a swing program, too large, can not finish.
  * */
@@ -7,7 +9,7 @@ public class TestJEdit extends AbstractSwingTest {
 
 	public String appPath = "D:\\research\\guierror\\subjects\\swing-programs\\jedit4.4.2.jar";
 	
-	public void testJEdit() {
+	public void testJEdit() throws IOException {
 		super.checkCallChainNumber(0, appPath, new String[]{"org"});
 	}
 }
