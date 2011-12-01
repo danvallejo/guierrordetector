f583668991b7e9ef62546f210af2091c28cffeb7
racezhang <racezhang@gmail.com>
2011-12-01 19:23:26 +0000

    Automates the process of extracting xml files

--- GUIErrorDetector/tests/edu/washington/cs/detector/util/TestApkUtils.java
+++ GUIErrorDetector/tests/edu/washington/cs/detector/util/TestApkUtils.java
@@ -1,18 +1,28 @@
 package edu.washington.cs.detector.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collection;
+import java.util.List;
+
 import junit.framework.TestCase;
 
 public class TestApkUtils extends TestCase {
 	
-	public void testExtractXML() {
+	public void testExtractXML() throws IOException {
 		ApkUtils.setApkToolDir("D:\\develop-tools\\apktool");
 		
 		String apkFile = "D:\\develop-tools\\apktool\\androidschool.cartoon.apk";
 		String extractDir = "D:\\develop-tools\\apktool\\tmp";
 		
 		String resultDir = ApkUtils.decryptXMFiles(apkFile, extractDir);
-		
 		assertEquals(resultDir, extractDir);
+		
+		List<Reader> readers = AndroidUtils.getAllLayoutXMLFromDir(new File(extractDir));
+		Collection<String> uis = AndroidUtils.extractAndroidUIs(readers);
+		
+		assertEquals(9, uis.size());
 		
 		ApkUtils.restoreToDefault();
 	}
