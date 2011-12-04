b3670744343dce19bbe957ff522df34891da2e3f
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2011-12-04 02:48:32 +0000

    tweak

--- trunk/GUIErrorDetector/src/edu/washington/cs/detector/guider/CGTraverseAndroidGuider.java
+++ trunk/GUIErrorDetector/src/edu/washington/cs/detector/guider/CGTraverseAndroidGuider.java
@@ -2,14 +2,25 @@ package edu.washington.cs.detector.guider;
 
 import com.ibm.wala.ipa.callgraph.CGNode;
 
+import edu.washington.cs.detector.util.Utils;
+
 public class CGTraverseAndroidGuider implements CGTraverseGuider {
 
 	//prune all system calls
 	CGTraverseGuider systemGuider = new CGTraverseNoSystemCalls();
 	
+	String[] safeMethods = new String[] {
+		"android.app.Dialog.show()V"
+	};
+	
 	@Override
 	public boolean traverse(CGNode src, CGNode dest) {
 		if(!systemGuider.traverse(src, dest)) {
+			return false;
+		}
+		
+		String destSig = dest.getMethod().getSignature();
+		if(Utils.includedIn(destSig, safeMethods)) {
 			return false;
 		}
 		
