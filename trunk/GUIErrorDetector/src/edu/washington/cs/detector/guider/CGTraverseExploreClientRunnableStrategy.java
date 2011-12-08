dbac106d1487f6899c2e90ba7027839d30038432
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2011-12-08 06:54:02 +0000

    code to reproduce the mytrack bugs

--- trunk/GUIErrorDetector/src/edu/washington/cs/detector/guider/CGTraverseExploreClientRunnableStrategy.java
+++ trunk/GUIErrorDetector/src/edu/washington/cs/detector/guider/CGTraverseExploreClientRunnableStrategy.java
@@ -6,14 +6,16 @@ import com.ibm.wala.ipa.callgraph.CGNode;
 
 import edu.washington.cs.detector.util.WALAUtils;
 
-/**
- * This is a very aggressive exploring strategy, buyer aware.
- * It only explore start -> runnable, where runnable is declared by client
- * */
-public class CGTraverseOnlyClientRunnableStrategy implements CGTraverseGuider {
+public class CGTraverseExploreClientRunnableStrategy implements CGTraverseGuider {
+	
+	public final String[] clientCode;
 	
 	CGTraverseGuider safeGuier = new CGTraverseAndroidSafeMethodGuider();
 	
+	public CGTraverseExploreClientRunnableStrategy(String[] clientCodePackage) {
+		this.clientCode = clientCodePackage;
+	}
+
 	@Override
 	public boolean traverse(CGNode src, CGNode dest) {
 		if(!safeGuier.traverse(src, dest)) {
@@ -27,18 +29,23 @@ public class CGTraverseOnlyClientRunnableStrategy implements CGTraverseGuider {
 		String methodName = srcMethod.getName().toString();
 		if(classFullName.equals("java.lang.Thread") && (methodName.equals("start") || methodName.equals("run"))) {
 			IClass destClass = dest.getMethod().getDeclaringClass();
-			String destPackage = WALAUtils.getJavaPackageName(destClass);
-			if(destPackage != null &&
-					(destPackage.startsWith("android.")
-							//|| destPackage.startsWith("com.google.")
-							|| destPackage.startsWith("com.android.")
-							|| destPackage.startsWith("java.lang."))) {
+			String destCode = WALAUtils.getJavaFullClassName(destClass);
+			if(!this.isInClientPackage(destCode)) {
 				//System.err.println("thread start: " + dest);
 				return false;
 			}
 		}
 		
 		return true;
+	}
+	
+	private boolean isInClientPackage(String destCode) {
+		for(String pName : this.clientCode) {
+			if(destCode.startsWith(pName)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
--- trunk/GUIErrorDetector/tests/edu/washington/cs/detector/experiments/android/TestMytracksAndroid.java
+++ trunk/GUIErrorDetector/tests/edu/washington/cs/detector/experiments/android/TestMytracksAndroid.java
@@ -1,19 +1,63 @@
 package edu.washington.cs.detector.experiments.android;
 
 import java.io.IOException;
+import java.util.List;
+
+import edu.washington.cs.detector.AnomalyCallChain;
+import edu.washington.cs.detector.CallChainFilter;
+import edu.washington.cs.detector.CGBuilder.CG;
+import edu.washington.cs.detector.experiments.filters.MergeSameEntryToStartPathStrategy;
+import edu.washington.cs.detector.guider.CGTraverseAndroidGuider;
+import edu.washington.cs.detector.guider.CGTraverseExploreClientRunnableStrategy;
+import edu.washington.cs.detector.guider.CGTraverseGuider;
+import edu.washington.cs.detector.guider.CGTraverseOnlyClientRunnableStrategy;
+import edu.washington.cs.detector.util.Globals;
+import edu.washington.cs.detector.util.Utils;
 
 public class TestMytracksAndroid extends AbstractAndroidTest {
 
 	@Override
 	protected String getAppPath() {
-		// TODO Auto-generated method stub
-		return null;
+		String appPath = "D:\\research\\guierror\\subjects\\android-programs\\sourcecode\\mytracks-april-29\\mytracks-april-29.jar"
+			+ Globals.pathSep +
+			"D:\\research\\guierror\\subjects\\android-programs\\sourcecode\\mytracks-april-29\\mytrackslib-april-29.jar"
+			+ Globals.pathSep +
+			"D:\\research\\guierror\\subjects\\android-programs\\sourcecode\\mytracks-april-29\\MyTracks\\libs\\accounts.jar"
+		    + Globals.pathSep +
+		    "D:\\research\\guierror\\subjects\\android-programs\\sourcecode\\mytracks-april-29\\MyTracks\\libs\\google-api-client-1.2.2-alpha.jar"
+		    + Globals.pathSep +
+		    "D:\\research\\guierror\\subjects\\android-programs\\sourcecode\\mytracks-april-29\\MyTracks\\libs\\google-common.jar"
+		    + Globals.pathSep +
+		    "D:\\research\\guierror\\subjects\\android-programs\\sourcecode\\mytracks-april-29\\MyTracks\\libs\\googleloginclient-helper.jar"
+		    +Globals.pathSep +
+		    "D:\\research\\guierror\\subjects\\android-programs\\sourcecode\\mytracks-april-29\\MyTracksLib\\libs\\protobuf-java-2.3.0-lite.jar"
+		    + Globals.pathSep + 
+			"D:\\research\\guierror\\eclipsews\\GUIErrorDetector\\exp-subjects\\original-android.jar"
+		    + Globals.pathSep +
+		    "D:\\Java\\android-sdk-windows\\add-ons\\addon-google_apis-google_inc_-8\\libs\\maps.jar";
+		return appPath;
 	}
 
 	@Override
 	protected String getDirPath() {
-		// TODO Auto-generated method stub
-		return null;
+		return "D:\\research\\guierror\\subjects\\android-programs\\sourcecode\\mytracks-april-29\\MyTracks";
+	}
+	
+	public void testFindErrors() {
+		CGTraverseGuider ui2startGuider = new CGTraverseAndroidGuider();
+		CGTraverseGuider start2checkGuider = new CGTraverseExploreClientRunnableStrategy(new String[]{"com.google.android.apps.mytracks.content.TrackDataHub$6"}); 
+			//new CGTraverseOnlyClientRunnableStrategy();
+		try {
+		    List<AnomalyCallChain> chains = super.findErrorsInAndroidApp(CG.RTA, ui2startGuider, start2checkGuider);
+		    int i = 0;
+		    for(AnomalyCallChain c : chains) {
+		    	System.out.println("The " + i++ + "-th chain:");
+			    System.out.println(c.getFullCallChainAsString());
+		    }
+		    Utils.dumpAnomalyCallChains(chains, "./output_chains.txt");
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void testUnzip() throws IOException {
