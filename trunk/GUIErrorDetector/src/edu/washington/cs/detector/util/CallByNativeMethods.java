7fb8a507a35f27df474ff96833bffd3cd9f105aa
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2012-01-03 18:21:41 +0000

    revise the declaration of the annotation

--- trunk/GUIErrorDetector/src/edu/washington/cs/detector/util/CallByNativeMethods.java
+++ trunk/GUIErrorDetector/src/edu/washington/cs/detector/util/CallByNativeMethods.java
@@ -1,14 +1,21 @@
 package edu.washington.cs.detector.util;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+
 /**
  * This annotation is used to decorate a method that may be called
  * by a native methods. For example:
  * 
- * @CallByNativeMethods(callers={"native1", "native2"})
+ * @CallByNativeMethods(callers={"ClassName.native1", "ClassName.native2"})
  * public void someMethodCalledByNatives() { ... }
  * 
  * */
 
-@interface CallByNativeMethods {
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface CallByNativeMethods {
 	String[] callers();
 }
