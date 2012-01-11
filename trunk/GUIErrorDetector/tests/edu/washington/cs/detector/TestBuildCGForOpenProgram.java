e60d7657b0b6d346537e10058bf8ca2d9ef15b97
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2012-01-11 01:57:36 +0000

    Test specifying customized types for entrypoints

--- trunk/GUIErrorDetector/tests/edu/washington/cs/detector/TestBuildCGForOpenProgram.java
+++ trunk/GUIErrorDetector/tests/edu/washington/cs/detector/TestBuildCGForOpenProgram.java
@@ -1,7 +1,9 @@
 package edu.washington.cs.detector;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedList;
 
 import com.ibm.wala.ipa.callgraph.Entrypoint;
 import com.ibm.wala.ipa.cha.ClassHierarchyException;
@@ -9,6 +11,7 @@ import com.ibm.wala.ipa.cha.ClassHierarchyException;
 import edu.washington.cs.detector.CGBuilder.CG;
 import edu.washington.cs.detector.util.PDFViewer;
 import edu.washington.cs.detector.util.Utils;
+import edu.washington.cs.detector.walaextension.ParamTypeCustomizedEntrypoint;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -33,6 +36,33 @@ public class TestBuildCGForOpenProgram extends TestCase {
 		builder.buildCG(entries);
 		
 		PDFViewer.viewCG("openprogram-orig.pdf", builder.getAppCallGraph());
+	}
+	
+	public void testBuildCGWithPreobjCreation() throws IOException, ClassHierarchyException {
+		String appPath = TestCommons.testfolder + "openprogram";
+		CGBuilder builder = new CGBuilder(appPath);
+		
+		builder.makeScopeAndClassHierarchy();
+		Iterable<Entrypoint> entries = CGEntryManager.getAllPublicMethods(builder, "test.openprogram.OpenProgram");
+		
+		//First set up user required class-to-be-instantiated
+		Collection<String> classesToInstantiate = new LinkedList<String>();
+		classesToInstantiate.add("test.openprogram.MooSub");
+		classesToInstantiate.add("test.openprogram.FooSub");
+		classesToInstantiate.add("test.openprogram.BarSub");
+		classesToInstantiate.add("test.openprogram.NooSub");
+		ParamTypeCustomizedEntrypoint.setUserClasses(classesToInstantiate);
+		
+		//convert it to param type customized entry point
+		entries = ParamTypeCustomizedEntrypoint.convertEntrypoints(entries);
+		
+		System.out.println("Number of entries to build CG: " + Utils.countIterable(entries));
+		System.out.println("All iterables: " + Utils.iterableToCollection(entries));
+		
+		builder.setCGType(CG.ZeroCFA);
+		builder.buildCG(entries);
+		
+		PDFViewer.viewCG("openprogram-orig-clazz.pdf", builder.getAppCallGraph());
 	}
 	
 }
