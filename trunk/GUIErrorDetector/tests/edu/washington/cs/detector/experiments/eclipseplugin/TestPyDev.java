7358c827922a6716249ae43e85fcd788e9446cba
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2012-01-19 01:58:39 +0000

    driver for pydev. it is too large, only RTA works

--- trunk/GUIErrorDetector/tests/edu/washington/cs/detector/experiments/eclipseplugin/TestPyDev.java
+++ trunk/GUIErrorDetector/tests/edu/washington/cs/detector/experiments/eclipseplugin/TestPyDev.java
@@ -3,48 +3,50 @@ package edu.washington.cs.detector.experiments.eclipseplugin;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import com.ibm.wala.classLoader.IClass;
-import com.ibm.wala.classLoader.IMethod;
 import com.ibm.wala.ipa.callgraph.CGNode;
 import com.ibm.wala.ipa.callgraph.Entrypoint;
-import com.ibm.wala.ipa.callgraph.impl.DefaultEntrypoint;
 import com.ibm.wala.ipa.cha.ClassHierarchy;
 import com.ibm.wala.ipa.cha.ClassHierarchyException;
-import com.ibm.wala.util.collections.Filtersection;
-import com.ibm.wala.util.collections.HashSetFactory;
-import com.ibm.wala.util.graph.Graph;
 
 import edu.washington.cs.detector.AnomalyCallChain;
-import edu.washington.cs.detector.CallChainFilter;
-import edu.washington.cs.detector.SWTAppUIErrorMain;
-import edu.washington.cs.detector.TestCommons;
-import edu.washington.cs.detector.UIAccessRunnableFinder;
 import edu.washington.cs.detector.UIAnomalyDetector;
 import edu.washington.cs.detector.CGBuilder.CG;
-import edu.washington.cs.detector.experiments.filters.RemoveSystemCallStrategy;
 import edu.washington.cs.detector.guider.CGTraverseSWTGuider;
 import edu.washington.cs.detector.util.EclipsePluginCommons;
 import edu.washington.cs.detector.util.EclipsePluginUtils;
 import edu.washington.cs.detector.util.Globals;
 import edu.washington.cs.detector.util.Log;
 import edu.washington.cs.detector.util.Utils;
-import edu.washington.cs.detector.util.WALAUtils;
-import edu.washington.cs.detector.walaextension.ParamTypeCustomizedEntrypoint;
 
-public class TestSubeclipseUI extends AbstractEclipsePluginTest {
-
-	public static String PLUGIN_DIR = 
-		// TestCommons.subeclipse_1_6 + Globals.fileSep 	+ "plugins"; //this is for 1.6.2
-		"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\subclipse-plugin-1.8.4\\plugins"; //for 1.8.4
+//project page: http://marketplace.eclipse.org/content/pydev-python-ide-eclipse
+public class TestPyDev extends AbstractEclipsePluginTest {
 	
-	private boolean init_all_arg_objs = false;
+	private String[] allPluginFiles = new String[] {
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\com.python.pydev.analysis_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\com.python.pydev.codecompletion_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\com.python.pydev.debug_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\com.python.pydev.fastparser_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\com.python.pydev.refactoring_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\com.python.pydev_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev.ast_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev.core_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev.customizations_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev.debug_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev.django_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev.django_templates_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev.help_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev.jython_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev.parser_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev.red_core_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev.refactoring_2.3.0.2011121518\\plugin.xml",
+			"D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\PyDev 2.3.0\\plugins\\org.python.pydev_2.3.0.2011121518\\plugin.xml"
+	};
 
 	@Override
 	protected String getAppPath() {
-		return PLUGIN_DIR;
+		return "D:\\research\\guierror\\subjects\\plugins-for-evaluate\\pydev\\extracted-jars";
 	}
 
 	@Override
@@ -56,20 +58,19 @@ public class TestSubeclipseUI extends AbstractEclipsePluginTest {
 	protected String getExtraJars() {
 		return "D:\\develop-tools\\eclipse\\eclipse\\plugins\\org.eclipse.core.resources_3.6.1.R36x_v20110131-1630.jar";
 	}
-
+	
 	@Override
 	protected Iterable<Entrypoint> getEntrypoints(ClassHierarchy cha) {
-		
 		//all declared
-		Collection<String> declaredClassesStrs = EclipsePluginUtils.getAllDeclaredClassesFromDir(getAppPath());
+		Collection<String> declaredClassesStrs = //EclipsePluginUtils.getAllDeclaredClassesFromDir(getAppPath());
+			Utils.extractClassFromPlugXMLFiles(allPluginFiles);
+		
 		Utils.dumpCollection(declaredClassesStrs, "./logs/declared_classes.txt");
 		Collection<IClass> declaredClasses = EclipsePluginUtils.getAllClasses(cha, declaredClassesStrs);
 		Collection<Entrypoint> entries = EclipsePluginUtils.getAllPublicProtectedEntypoints(declaredClasses, cha, false);
 		
 		//get all plugin methods
 		Collection<Entrypoint> pluginMethods = EclipsePluginUtils.getAllPluginMethodsAsEntrypoint(cha, super.getPackages());
-//		Utils.dumpCollection(pluginMethods, System.out);
-//		System.exit(1);
 		
 		Collection<IClass> jobClasses = EclipsePluginUtils.getAllJobClasses(cha, super.getPackages());
 		Collection<Entrypoint> jobMethods = EclipsePluginUtils.getAllPublicProtectedEntypoints(jobClasses, cha, false);
@@ -119,88 +120,22 @@ public class TestSubeclipseUI extends AbstractEclipsePluginTest {
 	protected Collection<CGNode> getStartNodes(Iterable<CGNode> allNodes,
 			ClassHierarchy cha) {
 		Collection<CGNode> startNodes = new HashSet<CGNode>();
-		
-		Collection<Entrypoint> actionEntrypoints = EclipsePluginUtils.getAllPublicProtectedActionEntrypoints(cha, super.getPackages(), false); //non-UI
-		Collection<Entrypoint> listenerEventHandler = EclipsePluginUtils.getAllPublicProtectedEventHandlerMethods(cha, super.getPackages(), false);
-		Collection<Entrypoint> jobChangeEntries = EclipsePluginUtils.getAllJobChangeListenerMethods(cha, super.getPackages(), false); //non-UI
-		
-		Set<String> methods = new HashSet<String>();
-		for(Entrypoint ep : actionEntrypoints) {
-			methods.add(WALAUtils.getFullMethodName(ep.getMethod()));
-		}
-		for(Entrypoint ep : listenerEventHandler) {
-			methods.add(WALAUtils.getFullMethodName(ep.getMethod()));
-		}
-		for(Entrypoint ep : jobChangeEntries) {
-			methods.add(WALAUtils.getFullMethodName(ep.getMethod()));
-		}
-		
-		System.out.println("The number of start nodes: " + methods.size());
-		
-		Graph<CGNode> graph = (Graph<CGNode>)allNodes;
-		
-		Collection<CGNode> runnables = UIAccessRunnableFinder.getAllAppRunMethods(graph, cha, super.getPackages());
-		System.out.println("Number of runnable: " + runnables.size());
-		
-		Utils.dumpCollection(runnables, System.out);
-		
-		Collection<IClass> jobClasses = EclipsePluginUtils.getAllJobClasses(cha, super.getPackages());
-		Utils.dumpCollection(jobClasses, System.out);
-		
-		System.out.println("See the job number.");
-		Collection<CGNode> jobRuns = EclipsePluginUtils.getAllJobRunMethods(graph, cha, super.getPackages());
-		Utils.dumpCollection(jobRuns, System.out);
-		
-		System.out.println("Resource listener number");
-		Collection<CGNode> resourceChangeMethods = EclipsePluginUtils.getAllResourceChangeMethods(graph, cha, super.getPackages());
-		Utils.dumpCollection(resourceChangeMethods, System.out);
-		
-		System.out.println("Action run number");
-		Collection<CGNode> actionMethods = EclipsePluginUtils.getAllActionRunMethods(graph, cha, super.getPackages());
-		Utils.dumpCollection(actionMethods, System.out);
-		//System.exit(0);
-		
-//		int i = 1;
-//		for(CGNode node : allNodes) {
-//			String methodCall = WALAUtils.getFullMethodName(node.getMethod());
-//			if(methods.contains(methodCall)) {
-//				i++;
-//				if(i < 100) {
-//					continue;
-//				}
-//				startNodes.add(node);
-//				if(i > 200) {
-//					break;
-//				}
-//			}
-//		}
-		
 		return startNodes;
 	}
-	
-	public void testAllClasses() {
-		Collection<String> clazzCol = EclipsePluginUtils.getAllDeclaredClassesFromDir(getAppPath());
-		System.out.println("Total num: " + clazzCol.size());
-		for(String c : clazzCol) {
-			System.out.println("  " + c);
-		}
-	}
 
-	public void testDetectUIErrors() throws IOException,
-	    ClassHierarchyException {
-		
+	public void testFindErrors() throws ClassHierarchyException, IOException {
 		Log.logConfig("./log.txt");
 		UIAnomalyDetector.DEBUG = true;
-		
-		this.init_all_arg_objs = true;
 		
 		super.setSeeUIAccessRunnable(true);
 		
 //		super.setCGType(CG.ZeroCFA);
 		super.setCGType(CG.OneCFA);
+		super.setCGType(CG.RTA);
+		super.setCGType(CG.ZeroCFA);
 		super.setThreadStartGuider(new CGTraverseSWTGuider());
 		super.setUIAnomalyGuider(new CGTraverseSWTGuider());
-		super.setPackages(new String[]{"org.tigris"});
+		super.setPackages(new String[]{"com.python.pydev", "org.python.pydev"});
 		
 		Collection<AnomalyCallChain> chains = super.reportUIErrors();
 		
@@ -210,6 +145,6 @@ public class TestSubeclipseUI extends AbstractEclipsePluginTest {
 			Log.logln(chain.getFullCallChainAsString());
 		}
 		System.out.println("Log: " + count + " output.");
-		
-    }
+	}
+
 }
