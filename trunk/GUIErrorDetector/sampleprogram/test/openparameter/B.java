60de9e9b4aeae452a1a786856bceacff7e7e05b4
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2012-01-27 03:33:16 +0000

    renaming

--- trunk/GUIErrorDetector/sampleprogram/test/openparameter/B.java
+++ trunk/GUIErrorDetector/sampleprogram/test/openparameter/B.java
@@ -1,27 +1,20 @@
 package test.openparameter;
 
-
-public class OpenParam {
-	
-	private A a;
-	
-	public OpenParam(Object o) {
-		a = new A(null);
-	}
-	
-	public void callFoo() {
-		a.foo();
-	}
-	
-}
-
 class Entry {
-	
-	public void callFoo2(OpenParam p) {
+	public void callFoo2(B p) {
 		p.callFoo();
 	}
 }
 
+public class B {
+	private A a;
+	public B(Object o) {
+		a = new A(null);
+	}
+	public void callFoo() {
+		a.foo();
+	}
+}
 class A {
 	public A(Object o) {}
 	public void foo() {}
