204b4fe451e031217642903751ade1765b1c9ec6
racezhang <racezhang@gmail.com>
2012-02-02 01:33:33 +0000

    create the native annotation processor

--- GUIErrorDetector/src/edu/washington/cs/detector/util/NativeAnnotationProcessor.java
+++ GUIErrorDetector/src/edu/washington/cs/detector/util/NativeAnnotationProcessor.java
@@ -1,8 +1,19 @@
 package edu.washington.cs.detector.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Collection;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Set;
+import java.util.zip.ZipException;
 
 import com.ibm.wala.classLoader.IClass;
 import com.ibm.wala.classLoader.IMethod;
@@ -11,20 +22,106 @@ import com.ibm.wala.ipa.cha.ClassHierarchy;
 import com.ibm.wala.shrikeCT.AnnotationsReader.UnimplementedException;
 import com.ibm.wala.shrikeCT.InvalidClassFileException;
 import com.ibm.wala.types.TypeName;
-import com.ibm.wala.types.annotations.Annotation;
-import com.ibm.wala.util.debug.Assertions;
 
 public class NativeAnnotationProcessor {
+	
+	public static Collection<String[]> getAllCallingPairsByNativeAnnotations(String jarFile, String lib) {
+		Collection<String[]> pairs = new LinkedList<String[]>();
+		try {
+		    Map<String, Collection<String>> relations = findCalledByNativesAnntationFromJarFile(jarFile, lib);
+		    for(String key : relations.keySet()) {
+		    	Collection<String> values = relations.get(key);
+		    	for(String value : values) {
+		    		String[] pair = new String[]{value, key};
+		    		pairs.add(pair);
+		    	}
+		    }
+		    return pairs;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
+	//java method  ->  a collection native method callers
+	public static Map<String, Collection<String>> findCalledByNativesAnntationFromJarFile(String jarFile, String lib) {
+		if(!jarFile.endsWith(".jar")) {
+			throw new RuntimeException("The file provided: " + jarFile + " is not a jar file.");
+		}
+		Map<String, Collection<String>> callRelations = new LinkedHashMap<String, Collection<String>>();
+		
+		try {
+			Set<Class<?>> classes = loadAllClasses(jarFile, lib);
+			for(Class<?> clz : classes) {
+				Method[] methods = clz.getDeclaredMethods();
+				for(Method m : methods) {
+					String methodSig = m.getDeclaringClass().getName() + "." + m.getName();
+					Annotation[] annotations = m.getAnnotations();
+					for(Annotation ann : annotations) {
+						if(ann.annotationType().equals(CalledByNativeMethods.class)) {
+							System.out.println("find annotation: " + ann);
+							CalledByNativeMethods nativeAnn = (CalledByNativeMethods)ann;
+							String[] callers = nativeAnn.callers();
+							if(callers.length != 0) {
+								if(!callRelations.containsKey(methodSig)) {
+									callRelations.put(methodSig, new LinkedHashSet<String>());
+								}
+								for(String caller : callers) {
+									callRelations.get(methodSig).add(caller);
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+		return callRelations;
+	}
+	
+	//FIXME a quick-dirty implementation, only allows 1 lib.
+	public static Set<Class<?>> loadAllClasses(String jarFile, String lib) throws ClassNotFoundException, ZipException, IOException {
+		File file  = new File(jarFile);
+		File libFile = new File(lib);
+		URL[] urls = new URL[]{file.toURL(), libFile.toURL()};
+		ClassLoader cl = new URLClassLoader(urls);
+
+		Set<Class<?>> loadedClasses = new LinkedHashSet<Class<?>>();
+		Collection<String> contents = JarViewer.getContentsAsStr(file);
+		//Class<?> cls = cl.loadClass("com.mypackage.myclass");
+		
+		for(String content : contents) {
+			if(content.endsWith(".class")) {
+				//System.out.println(content);
+				String className = content.substring(0, content.length() - ".class".length());
+				//System.out.println(className);
+				className = Utils.translateSlashToDot(className);
+				//System.out.println(className);
+				Class<?> cls = cl.loadClass(className);
+				loadedClasses.add(cls);
+			}
+		}
+		
+		return loadedClasses;
+	}
+	
+	/**
+	 * The below code uses WALA to read the annotation. However, as of Dec 2011, WALA does not
+	 * support method annotation yet, although a late commit indicates the annotation support
+	 * has been added:
+	 * http://sourceforge.net/mailarchive/forum.php?thread_name=OFA74890B8.9E3755EA-ON87257982.
+	 * 006305F0-87257982.006315AE%40us.ibm.com&forum_name=wala-wala
+	 * 
+	 * For the sake of reducing potential risk, the above Java reflection-based approach
+	 * is still being used.
+	 * */
 	public static Map<String, Collection<String>> getNativeCallRelations(
 			ClassHierarchy cha) throws InvalidClassFileException,
 			UnimplementedException {
-
 		Map<String, Collection<String>> callRelations = new LinkedHashMap<String, Collection<String>>();
-		
 		TypeName nativeAnnotation = TypeName.findOrCreateClassName(
 				"edu/washington/cs/detector/util", "CallByNativeMethods");
-
 		for (IClass clz : cha) {
 			Collection<IMethod> methods = clz.getDeclaredMethods();
 			for (IMethod method : methods) {
@@ -33,9 +130,7 @@ public class NativeAnnotationProcessor {
 							nativeAnnotation);
 					if (ann != null) {
 						System.out.println(" method: " + method);
-						
 						throw new RuntimeException("No idea in how to get annotation values!");
-						
 					}
 				} else {
 					System.err.println("WARNING: see a non-ShrikeCTMethod: "
@@ -48,23 +143,23 @@ public class NativeAnnotationProcessor {
 	}
 	
 	public static Annotation getVisibleAnnotation(IMethod m, TypeName type) {
-	    if (m instanceof ShrikeCTMethod) {
-	      Collection<Annotation> annotations = null;
-	      try {
-	        annotations = ((ShrikeCTMethod) m).getRuntimeVisibleAnnotations();
-	      } catch (InvalidClassFileException e) {
-	        e.printStackTrace();
-	        Assertions.UNREACHABLE();
-	      } catch (UnimplementedException e) {
-	        e.printStackTrace();
-	        Assertions.UNREACHABLE();
-	      }
-	      for (Annotation a : annotations) {
-	        if (a.getType().getName().equals(type)) {
-	          return a;
-	        }
-	      }
-	    }
+//	    if (m instanceof ShrikeCTMethod) {
+//	      Collection<Annotation> annotations = null;
+//	      try {
+//	        annotations =((ShrikeCTMethod) m).getRuntimeVisibleAnnotations();
+//	      } catch (InvalidClassFileException e) {
+//	        e.printStackTrace();
+//	        Assertions.UNREACHABLE();
+//	      } catch (UnimplementedException e) {
+//	        e.printStackTrace();
+//	        Assertions.UNREACHABLE();
+//	      }
+//	      for (Annotation a : annotations) {
+//	        if (a.getType().getName().equals(type)) {
+//	          return a;
+//	        }
+//	      }
+//	    }
 	    return null;
 	  }
 }
--- GUIErrorDetector/tests/edu/washington/cs/detector/util/TestNativeAnnotationProcessor.java
+++ GUIErrorDetector/tests/edu/washington/cs/detector/util/TestNativeAnnotationProcessor.java
@@ -1,6 +1,10 @@
 package edu.washington.cs.detector.util;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.zip.ZipException;
 
 import com.ibm.wala.core.tests.callGraph.CallGraphTestUtil;
 import com.ibm.wala.ipa.cha.ClassHierarchy;
@@ -25,4 +29,38 @@ public class TestNativeAnnotationProcessor extends TestCase {
 		NativeAnnotationProcessor.getNativeCallRelations(cha);
 	}
 	
+
+	//using the Java reflection approach
+	public void testNativeAnnotation() {
+		String jarFileName = //"D:\\research\\guierror\\subjects\\android-programs\\extracted\\sgtpuzzles.jar";
+		    "D:\\research\\guierror\\subjects\\android-programs\\extracted\\sgtpuzzles-annotation.jar";
+		
+		String lib = "D:\\research\\guierror\\eclipsews\\GUIErrorDetector\\exp-subjects\\original-android.jar";
+		try {
+			Set<Class<?>> loadedClasses = NativeAnnotationProcessor.loadAllClasses(jarFileName, lib);
+			System.out.println("Number of loaded class: " + loadedClasses.size());
+			
+			//get the relations
+			Map<String, Collection<String>> relations
+			    = NativeAnnotationProcessor.findCalledByNativesAnntationFromJarFile(jarFileName, lib);
+			System.out.println(relations);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void testNativeMethodPairs() {
+		String jarFileName = //"D:\\research\\guierror\\subjects\\android-programs\\extracted\\sgtpuzzles.jar";
+		    "D:\\research\\guierror\\subjects\\android-programs\\extracted\\sgtpuzzles-annotation.jar";
+		
+		String lib = "D:\\research\\guierror\\eclipsews\\GUIErrorDetector\\exp-subjects\\original-android.jar";
+		try {
+			Collection<String[]> pairs = NativeAnnotationProcessor.getAllCallingPairsByNativeAnnotations(jarFileName, lib);
+			for(String[] pair : pairs) {
+				System.out.println(pair[0] + "  -> " + pair[1]);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
