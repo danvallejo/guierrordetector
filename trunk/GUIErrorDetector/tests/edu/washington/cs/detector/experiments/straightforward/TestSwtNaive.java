5aa6d45d86fdbe70f61024c251053401ed140c1d
racezhang@gmail.com <racezhang@gmail.com@bafd9c19-8dcb-4ca2-9c4f-e0dd673b95ae>
2012-02-07 02:20:35 +0000

    Running requiring wrappers for ArecaBackup

--- trunk/GUIErrorDetector/tests/edu/washington/cs/detector/experiments/straightforward/TestSwtNaive.java
+++ trunk/GUIErrorDetector/tests/edu/washington/cs/detector/experiments/straightforward/TestSwtNaive.java
@@ -61,4 +61,39 @@ public class TestSwtNaive extends TestCase {
 		Utils.dumpCollection(unsafe, "./logs/filebunkernaive.txt");
 	}
 	
+	public void testArecaBackup() throws IOException {
+		String dir = "D:\\research\\guierror\\subjects\\swt-subjects\\areca-latest\\";
+		
+	    String appPath = dir + "areca-latest.jar";
+	    
+		String libJar = dir + "activation.jar"
+		    + Globals.pathSep + dir + "commons-net-1.4.1.jar"
+		    + Globals.pathSep + dir +  "jakarta-oro-2.0.8.jar"
+		    + Globals.pathSep + dir + "jsch.jar"
+		    + Globals.pathSep + dir +  "local_policy.jar"
+		    + Globals.pathSep + dir +  "mail.jar"
+		    + Globals.pathSep + dir +  "org.eclipse.core.commands_3.2.0.I20060605-1400.jar"
+		    + Globals.pathSep + dir +  "org.eclipse.equinox.common_3.2.0.v20060603.jar"
+		    + Globals.pathSep + dir +  "org.eclipse.jface_3.2.0.I20060605-1400.jar"
+	        + Globals.pathSep + "D:\\develop-tools\\eclipse\\eclipse\\plugins\\org.eclipse.swt.win32.win32.x86_64_3.6.2.v3659c.jar";
+
+		String path = appPath + Globals.pathSep + libJar;
+		
+		// configure the call graph builder, use 1-CFA as default
+		CGBuilder builder = new CGBuilder(path);
+		builder.setCGType(CG.RTA);
+		builder.buildCG();
+		
+        String[] packages = new String[]{"com.application.areca", "com.jcraft.jsch", "com.myJava"};
+		
+		//check the straightforward approach result
+		UnsafeUIAccessMethodFinder finder = new UnsafeUIAccessMethodFinder(builder.getAppCallGraph(), packages);
+		finder.setClassHierarchy(builder.getClassHierarchy());
+		finder.setUIAccessDecider(GUI.SWT);
+		Collection<IMethod> unsafe = finder.finalAllUnsafeUIAccessMethods();
+		
+		System.out.println("No of unsafe: " + unsafe.size());
+		Utils.dumpCollection(unsafe, "./logs/arecabackupnaive.txt");
+	}
+	
 }
