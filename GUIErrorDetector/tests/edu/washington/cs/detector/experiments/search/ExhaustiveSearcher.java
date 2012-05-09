10b2085fbae786d90c00bc32248f17298d4b341c
racezhang <racezhang@gmail.com>
2012-05-09 21:24:09 +0000

    add comments for the exhaustive searcher class

--- GUIErrorDetector/tests/edu/washington/cs/detector/experiments/search/ExhaustiveSearcher.java
+++ GUIErrorDetector/tests/edu/washington/cs/detector/experiments/search/ExhaustiveSearcher.java
@@ -13,27 +13,52 @@ import edu.washington.cs.detector.NativeMethodConnector;
 import edu.washington.cs.detector.guider.CGTraverseDefaultGuider;
 import edu.washington.cs.detector.guider.CGTraverseGuider;
 
+/**
+ * The basic algorithm to perform exhaustive search of all
+ * non-cyclic paths on a graph.
+ * */
 abstract public class ExhaustiveSearcher {
 	
+	/**
+	 * If set, stop searching when a certain amount of time reaches
+	 * also see class SimpleClock for reference
+	 * */
 	public static boolean USE_CLOCK = false;
 	
+	/**
+	 * The graph on which the search is performed
+	 * */
 	public final Graph<CGNode> graph;
 	
+	/**
+	 * The starting node for a search
+	 * */
 	public final CGNode startNode;
 	
+	/**
+	 * Customize graph search strategies if needed, e.g., which succ
+	 * nodes to search first. By default, it does nothing.
+	 * */
 	CGTraverseGuider guider = new CGTraverseDefaultGuider();
 	
+	/**
+	 * This connector represents the calling relationship between a native method
+	 * and a Java method. It is initialized by reading the CalledByNativeMethods
+	 * annotations for programs using native methods.
+	 * By default, it does nothing.
+	 * */
 	protected NativeMethodConnector connector = NativeMethodConnector.createEmptyConnector();
 	
+	/**
+	 * Constructors, and several simple setters
+	 * */
 	public ExhaustiveSearcher(Graph<CGNode> graph, CGNode startNode) {
 		this.graph = graph;
 		this.startNode = startNode;
 	}
-	
 	public void setTraverseGuider(CGTraverseGuider guider) {
 		this.guider = guider;
 	}
-	
 	public void setNativeMethodConnector(NativeMethodConnector connector) {
 		if(connector == null) {
 			throw new RuntimeException("The native connector can not be null.");
@@ -41,52 +66,80 @@ abstract public class ExhaustiveSearcher {
 		this.connector = connector;
 	}
 	
+	/**
+	 * A method checking whether the current node is a target node or not.
+	 * It must be overriden by subclasses. For example, when searching all
+	 * paths from starting node to all thread-spawning node. This method
+	 * returns true if node is "Thread.start()".
+	 * */
+	abstract protected boolean isDestNode(CGNode node);
+	
+	/**
+	 * Performs breadth-first exhaustive search. It returns a collection of
+	 * paths (called chains below). Here a path is a list of CGNode.
+	 * 
+	 * Note that, this is a recursive algorithm. Initially, clients use:
+	 * 
+	 *   breadFirst(graph, new LinkedList<CGNode>());  //empty visited nodes at the beginning
+	 *   
+	 * */
 	public Collection<List<CGNode>> breadFirst(Graph<CGNode> graph, LinkedList<CGNode> visited) {
 		Collection<List<CGNode>> returnChains = new LinkedHashSet<List<CGNode>>();
-		
+		//return the current visited chains if time is up
 		if(USE_CLOCK) {
         	if(SimpleClock.finish()) {
         		return returnChains;
         	}
         }
-		
+		//fetch one from the visited list, and get all adjacent nodes.
 		CGNode lastNode = visited.getLast();
-		LinkedList<CGNode> nodes = this.getAdjacentNodes(graph, lastNode);// graph.adjacentNodes(visited.getLast());
+		LinkedList<CGNode> ajacentNodes = this.getAdjacentNodes(graph, lastNode);
+		//check whether that it is a native method or not
+		//if it is native method, check whether this native method is annotated to be called
+		//by java methods
 		if(lastNode.getMethod().isNative()) {
-			if(!nodes.isEmpty()) {
-				throw new RuntimeException("Must be empty.");
+			if(!ajacentNodes.isEmpty()) {
+				throw new RuntimeException("The visited nodes can not be empty. A native method" +
+						" can not be called directly by client.");
 			}
+			//check the CalledByNativeMethods annotations
 			if(!this.connector.isEmpty()) {
-				nodes.addAll(this.connector.getSucc(graph, lastNode));
+				ajacentNodes.addAll(this.connector.getSucc(graph, lastNode));
 			}
 		}
-		
-        // examine adjacent nodes
-        for (CGNode node : nodes) {
+        // examine each ajacentNode one by one
+		// note the following code does NOT add node to visited, it just checks
+        for (CGNode node : ajacentNodes) {
+        	//skip if already visited
             if (visited.contains(node)) {
                 continue;
             }
+            //a customized searching strategy. do nothing at default,
+            //for debugging purpose.
             if(!this.shouldVisit(lastNode, node)) {
             	continue;
             }
+            //if it is the target node
             if (this.isDestNode(node)) {
                 visited.add(node);
-//                printPath(visited);
+                //create the path
                 List<CGNode> chain = this.generatePath(visited);
                 returnChains.add(chain);
-                
+                //return if time up
                 if(USE_CLOCK) {
                 	if(SimpleClock.finish()) {
                 		return returnChains;
                 	}
                 }
-                
                 visited.removeLast();
                 break;
             }
         }
-        // in breadth-first, recursion needs to come after visiting adjacent nodes
-        for (CGNode node : nodes) {
+        /**
+         * The following is the critical recursion code.
+         * Doing recursion AFTER visiting adjacent nodes
+         * */
+        for (CGNode node : ajacentNodes) {
             if (visited.contains(node) || this.isDestNode(node)) {
                 continue;
             }
@@ -95,17 +148,18 @@ abstract public class ExhaustiveSearcher {
             		return returnChains;
             	}
             }
-            
             visited.addLast(node);
-//            breadthFirst(graph, visited);
+            //do recursive below
             returnChains.addAll(breadFirst(graph, visited));
-            
             visited.removeLast();
         }
         
         return returnChains;
 	}
 	
+	/**
+	 * Gets all adjacent nodes in a graph
+	 * */
 	private LinkedList<CGNode> getAdjacentNodes(Graph<CGNode> graph, CGNode node) {
 		LinkedList<CGNode> l = new LinkedList<CGNode>();
 		Iterator<CGNode> it = graph.getSuccNodes(node);
@@ -115,16 +169,20 @@ abstract public class ExhaustiveSearcher {
 		return l;
 	}
 	
-	abstract protected boolean isDestNode(CGNode node);
-	
+	/**
+	 * Check if the edge srcNode -> destNode should be visited or not.
+	 * By default, it always returns true, i.e., no effect of pruning.
+	 * */
 	private boolean shouldVisit(CGNode srcNode, CGNode destNode) {
 		return guider.traverse(srcNode, destNode);
 	}
 	
+	/**
+	 * Dumps the visited nodes in sequence and returns a path
+	 * */
 	private List<CGNode> generatePath(LinkedList<CGNode> visited) {
 		List<CGNode> returned = new LinkedList<CGNode>();
 		returned.addAll(visited);
 		return returned;
 	}
-	
 }
